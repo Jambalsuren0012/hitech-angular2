36c09d126cfc1804a718c0b44a3480f0bf1b130c
Erdenebat <jerdenebat19@gmail.com>
2024-12-18 19:00:35 +0800

    lang change 1

--- src/app/pages/all-books/all-books.component.ts
+++ src/app/pages/all-books/all-books.component.ts
@@ -1,61 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../service/books.service';
 import { environment } from '../../environments/environment';
+import { TranslateServiceService } from '../../service/translate-service.service';
 
 @Component({
-  selector: 'app-all-books',
-  templateUrl: './all-books.component.html',
-  styleUrls: ['./all-books.component.css'],
+ selector: 'app-all-books',
+ templateUrl: './all-books.component.html',
+ styleUrls: ['./all-books.component.css'],
 })
 export class AllBooksComponent implements OnInit {
-  bookData: Array<any> = [];
-  itemsPerPage = 15;
-  currentPage = 1;
-  imageUrl = environment.imgUrl;
+ bookData: Array<any> = [];
+ itemsPerPage = 15;
+ currentPage = 1;
+ imageUrl = environment.imgUrl;
+ lang = 'mn';
 
-  constructor(private booksService: BooksService) {}
+ constructor(
+  private booksService: BooksService,
+  private language: TranslateServiceService
+ ) {}
 
-  get paginatedItems() {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.bookData.slice(startIndex, endIndex);
-  }
+ get paginatedItems() {
+  const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+  const endIndex = startIndex + this.itemsPerPage;
+  return this.bookData.slice(startIndex, endIndex);
+ }
 
-  get totalPages() {
-    return Math.ceil(this.bookData.length / this.itemsPerPage);
-  }
+ get totalPages() {
+  return Math.ceil(this.bookData.length / this.itemsPerPage);
+ }
 
-  changePage(page: number) {
-    this.currentPage = page;
-  }
+ changePage(page: number) {
+  this.currentPage = page;
+ }
 
-  nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-    }
+ nextPage() {
+  if (this.currentPage < this.totalPages) {
+   this.currentPage++;
   }
+ }
 
-  previousPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-    }
+ previousPage() {
+  if (this.currentPage > 1) {
+   this.currentPage--;
   }
+ }
 
-  fetchAllBook() {
-    this.booksService.getAllBook().subscribe(
-      (data: any) => {
-        this.bookData = data;
-      },
-      (error) => {
-        console.error('Error fetching books:', error);
-      },
-    );
-  }
+ fetchAllBook() {
+  this.booksService.getAllBook(this.lang).subscribe(
+   (data: any) => {
+    this.bookData = data;
+   },
+   (error) => {
+    console.error('Error fetching books:', error);
+   }
+  );
+ }
 
-  ngOnInit() {
-    this.fetchAllBook();
-  }
-  trackById(index: number, data: any): string {
-    return data.id;
-  }
+ ngOnInit() {
+  this.language.loadLang.subscribe((lang: any) => {
+   this.lang = lang;
+   this.fetchAllBook();
+  });
+ }
+ trackById(index: number, data: any): string {
+  return data.id;
+ }
 }
